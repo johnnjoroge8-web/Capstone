b81bc812da719ddef827fa51be53bbea055d34cb
johnnjoroge8-web <john.njoroge8@student.moringaschool.com>
2025-10-29 22:03:33 +0400

    Add files via upload

---
+++ sampproj.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //SAMPLE PROJECT TO; calculate the surface area and volume of a sphere
+
+ double radius = 0.0;
+ double area = 0.0;
+ double surfacearea = 0.0;
+ double volume = 0.0;
+ const double PI = 3.14159;
+
+ printf("Enter the radius: ");
+    scanf("%lf", &radius);
+
+    area = PI *pow(radius, 2);
+    surfacearea = 4 * PI * pow(radius, 2);
+    volume =(4.0/3.0) * PI *pow(radius, 3);
+    
+
+    printf("Area: %.2lf\n", area);
+
+    printf("Surface Area: %.2lf\n", surfacearea);
+
+    printf("Volume: %.2lf\n", volume);
+    
+    return 0;
+
+}
